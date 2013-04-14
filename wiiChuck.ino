6d948263bea45fbcb775adfa1dd1f253bb5d7b70
Erich Nachbar <erich@stuffylab.com>
2013-04-13 17:50:27 -0700

    well working version with ramping

--- wiiChuck.ino
+++ wiiChuck.ino
@@ -32,14 +32,14 @@ double angle;
 
 void setup() {
 
-//  // check if we have ever written into it. if not initialize it with default.
-//  int init_div = EEPROM.read(DIV_ADDR);
-//  if( init_div > 20 || init_div <= 0 ){
-//    EEPROM.write(DIV_ADDR, div);
-//  } else {
-//    // we have written it before. restore it so it survives restarts.
-//     div =  EEPROM.read(DIV_ADDR);
-//  }
+  //  // check if we have ever written into it. if not initialize it with default.
+  //  int init_div = EEPROM.read(DIV_ADDR);
+  //  if( init_div > 20 || init_div <= 0 ){
+  //    EEPROM.write(DIV_ADDR, div);
+  //  } else {
+  //    // we have written it before. restore it so it survives restarts.
+  //     div =  EEPROM.read(DIV_ADDR);
+  //  }
 
   myservo.attach(9);  // attaches the servo on pin 9 to the servo object 
   myservo2.attach(10);  // attaches the servo on pin 9 to the servo object 
@@ -51,14 +51,15 @@ void setup() {
 
 float convY(int chuck){ 
 
-  return 90.6185+0.9278*(float)chuck;
+  //0.7025*97+93.9008
+  return 96.0029+0.6696*(float)chuck;
 
-//  if(chuck>0){
-//    return (float)(90+chuck*(90.0/110.0));
-//  } 
-//  else {
-//    return (float)(90.0+chuck*(90.0/102.0));
-//  } 
+  //  if(chuck>0){
+  //    return (float)(90+chuck*(90.0/110.0));
+  //  } 
+  //  else {
+  //    return (float)(90.0+chuck*(90.0/102.0));
+  //  } 
 }
 
 
@@ -75,6 +76,9 @@ float convX(int chuck){
 //  } 
 //}
 
+int currY = 90;
+int currX = 90;
+
 void chuck2servo(){
   delay(50);
   chuck.update(); 
@@ -84,14 +88,57 @@ void chuck2servo(){
     // if still pressed increase sensitity
     chuck.update();
     if(chuck.zPressed() && chuck.cPressed()){
-//      incDiv();    
+      //      incDiv();    
     }    
   }
 
-  posY = chuck.readJoyY();
-  posX = chuck.readJoyX();
+  if(!chuck.zPressed()){
+    posY = 0;
+    posX = 0;
+  } 
+  else {
+    posY = chuck.readJoyY();
+    posX = chuck.readJoyX();
+  }
+
   sYPos = convY(posY);
   sXPos = convX(posX);
+
+  //
+  int RMPINC = 5;
+  int RMPINCFAST = 7;
+
+  // slow ramp
+  if(currY <= sYPos){
+    sYPos = currY + RMPINC;
+    currY = sYPos;
+  } 
+  else {
+    sYPos = currY - RMPINC;
+    currY = sYPos;
+  }
+
+  //  if(posX == 0){
+  if(currX <= sXPos){
+    sXPos = currX + RMPINCFAST;
+    currX = sXPos;
+  } 
+  else {
+    sXPos = currX - RMPINCFAST;
+    currX = sXPos;
+  }
+
+  // filter stop jitter 
+  if(sYPos > 94 && sYPos < 101){
+    sYPos = 90;
+  }
+
+  if(sXPos > 89 && sXPos < 101){
+    sXPos = 90;
+  }
+
+  if(sYPos>164) sYPos = 164;
+  if(sYPos<27) sYPos = 27;
 
   Serial.print("y:");
   Serial.print(posY);
@@ -107,14 +154,8 @@ void chuck2servo(){
   Serial.print(chuck.cPressed()?"c_on ":"c_off");
   Serial.println();
 
-  if(chuck.zPressed()){
-    myservo.write(sYPos);
-    myservo2.write(sXPos);
-  } 
-  else {
-    myservo.write(90);
-    myservo2.write(90);
-  }  
+  myservo.write(sYPos);
+  myservo2.write(sXPos);
 }
 
 void loop() {
@@ -122,6 +163,14 @@ void loop() {
   chuck2servo();  
 
 }
+
+
+
+
+
+
+
+
 
 
 
